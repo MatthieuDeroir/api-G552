cda757cc552e027cfe7c37f0662affba94f4415c
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-01-12 13:57:04 +0100

    Routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const UserController = require('../controllers/userController');
+
+const router = express.Router();
+const userController = new UserController();
+
+router.post('/', userController.create);
+router.put('/:id', userController.update);
+router.get('/', userController.getAll);
+router.get('/:id', userController.getById);
+router.delete('/:id', userController.delete);
+
+module.exports = router;
