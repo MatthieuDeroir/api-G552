704a32b1a0cbd93c8be01e00281f2f9819811c05
GuillaumeLeGoff <legoff.guill@gmail.com>
2023-02-06 17:18:25 +0100

    .

--- Controllers/eventmediaController.js
+++ Controllers/eventmediaController.js
@@ -17,7 +17,7 @@ class EventMediaController {
     }
 
     getAllByEvent = (req, res) => {
-        this.eventmedia.getAllByMedia(req.params.eventId)
+        this.eventmedia.getAllByEvent(req.params.eventId)
             .then((eventmedias) => {
                 res.status(200).json(eventmedias);
             })
