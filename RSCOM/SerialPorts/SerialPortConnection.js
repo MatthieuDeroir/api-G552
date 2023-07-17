8f25e4be72f1eb7b79f78efab127d3c61df242d5
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-07-17 16:53:55 +0200

    async and error handling

--- RSCOM/SerialPorts/SerialPortConnection.js
+++ RSCOM/SerialPorts/SerialPortConnection.js
@@ -101,7 +101,7 @@ class SerialPortConnection {
     async ConnectAvailablePorts() {
         for (const device of AllDevices) {
             if (!device.Started && device.DeviceExists) {
-                console.log("Connecting to : " + device.DevicePortName + ' ');
+                // console.log("Connecting to : " + device.DevicePortName + ' ');
                 device.Started = true;
 
                 const options = {
