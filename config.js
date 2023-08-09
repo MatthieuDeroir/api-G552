a574b6fb904800901efbea9ec56b6054bc9da58d
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-08-09 13:54:53 +0200

    baudrate set to 19200

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const config = {
     SerialPort: {
         MaxRetries: 10,
         RefreshInterval: 100,
-        BaudRate: 115200,
+        BaudRate: 19200,
         DataBits: 8,
         Parity: 'none',
         StopBits: 1,
