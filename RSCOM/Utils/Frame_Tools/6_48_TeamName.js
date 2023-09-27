dd80c4bb4f1b64839b3a267d5fb115559a3e3f87
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-09-27 17:31:11 +0200

    removing debug logs

--- RSCOM/Utils/Frame_Tools/6_48_TeamName.js
+++ RSCOM/Utils/Frame_Tools/6_48_TeamName.js
@@ -2,9 +2,10 @@ const StringDecoder = require('string_decoder').StringDecoder;
 
 function TeamName(startIndex, _message) {
     const UTF16 = new StringDecoder('utf16le');
+    console.log(_message.slice(startIndex, startIndex + 18));
+
     return UTF16.write(Buffer.from(_message.slice(startIndex, startIndex + 18)));
 
-    console.log(_message.slice(startIndex, startIndex + 18));
 }
 
 module.exports = TeamName;
