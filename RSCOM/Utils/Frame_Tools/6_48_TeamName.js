3a64394f4d868b2bb297bbc3d7472c486eb7cef4
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-10-09 11:42:03 +0200

    debugging

--- RSCOM/Utils/Frame_Tools/6_48_TeamName.js
+++ RSCOM/Utils/Frame_Tools/6_48_TeamName.js
@@ -9,7 +9,13 @@ function TeamName(startIndex, _message) {
     const slicedBuffer = buffer.slice(startIndex, startIndex + 18 * 4);
 
     // Decode using utf32le with iconv-lite
-    const decodedMessage = iconv.decode(slicedBuffer, 'utf-32le');
+    const decodedMessage = iconv.decode(slicedBuffer, 'utf32le');
+
+    const iconv = require('iconv-lite');
+
+    // Check if utf32le is supported by iconv-lite
+    console.log(iconv.encodingExists('utf32le'));
+
 
     console.log(decodedMessage);
     return decodedMessage;
