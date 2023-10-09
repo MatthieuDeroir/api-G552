15ee9e8d41a99e0975d5715b407cf9aee9a27783
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-10-09 09:49:03 +0200

    Mode : message[2]

--- Sockets/Unixsocket.js
+++ Sockets/Unixsocket.js
@@ -162,6 +162,7 @@ const server = net.createServer((client) => {
 
     client.on('data', (data) => {
         try {
+            console.log('Received raw data:', data)
             const parsedData = JSON.parse(data.toString());
             sharedEmitter.emit('data-received', parsedData);
         } catch (err) {
