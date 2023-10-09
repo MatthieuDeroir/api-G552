154d9a3e698d30f76abb5d1187212047383ff073
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-10-09 10:50:55 +0200

    debugging

--- Sockets/Unixsocket.js
+++ Sockets/Unixsocket.js
@@ -144,7 +144,7 @@ const server = net.createServer((client) => {
                 client.write(JSON.stringify(data) + '\n');
             } else {
                 client.write(JSON.stringify({ mode: 'diaporama', path: data.path, duration: data.duration }) + '\n');
-                console.log('Sent diaporama data:', data);
+                // console.log('Sent diaporama data:', data);
             }
         } catch (err) {
             console.error('Failed to send data:', err);
