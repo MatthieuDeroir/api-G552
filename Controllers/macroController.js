b995b962c71e448e5ee872dd7ccf0fc185c68c13
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-10-10 16:52:09 +0200

    debugging

--- Controllers/macroController.js
+++ Controllers/macroController.js
@@ -26,7 +26,7 @@ class MacroController {
 
         const allActiveSessions = await this.activeSession.getAll();
 
-        // console.log("allActiveSessions", allActiveSessions)
+        console.log("allActiveSessions", allActiveSessions)
 
         // 1. Vérifier la session active
         const activeSession = await this.activeSession.getFirst();
