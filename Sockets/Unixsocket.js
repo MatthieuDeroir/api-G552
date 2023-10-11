b8fa5ed571ad17488ce81bf009b1841d6b7d1d66
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-10-11 11:06:25 +0200

    debugging

--- Sockets/Unixsocket.js
+++ Sockets/Unixsocket.js
@@ -157,7 +157,6 @@ const server = net.createServer((client) => {
 
                 // console.log('Sent score gameState', data)
             }else if (data?.Mode === 0,1,2,16,17,18,19,20){
-                console.log("bloup");
                 client.write(JSON.stringify(data) + '\n');
             } else {
                 client.write(JSON.stringify(data) + '\n');
