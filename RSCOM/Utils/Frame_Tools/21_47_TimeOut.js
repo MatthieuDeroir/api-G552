1d0604af946de8f1135235b626b202ca16d019f9
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-10-11 13:51:05 +0200

    debugging

--- RSCOM/Utils/Frame_Tools/21_47_TimeOut.js
+++ RSCOM/Utils/Frame_Tools/21_47_TimeOut.js
@@ -1,7 +1,7 @@
 const nBytesToNumber = require("../nBytesToNumber");
 
 function TimeOut(digit1, digit2, digit3) {
-    return digit1.toString() + nBytesToNumber(digit2).toString() + "." + nBytesToNumber(digit3).toString();
+    return digit1.toString() + digit2.toString() + "." + digit3.toString();
 
 }
 
