25514e8451d645072e3c473a750fe2e1923ef509
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-10-11 10:47:49 +0200

    debugging

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ const config = {
         StopBits: 1,
         Handshake:'none',
         Path:`/dev`,
-        Filter: `ttyFIQ`,
+        Filter: `ttyS0`,
         ReadTimeout: 2000,
         WriteTimeout: 2000,
     }
