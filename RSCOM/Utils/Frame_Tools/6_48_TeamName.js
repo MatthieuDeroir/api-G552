d3fccf6197071feb25fde5397e2b992cf8760008
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-10-18 12:58:00 +0200

    debugging

--- RSCOM/Utils/Frame_Tools/6_48_TeamName.js
+++ RSCOM/Utils/Frame_Tools/6_48_TeamName.js
@@ -11,8 +11,12 @@ function TeamName(startIndex, _message) {
     // const slicedBuffer = buffer.slice(startIndex, startIndex + 18 * 2);
 
     // Decode using utf32le with iconv-lite
-    // remove any character that is not a letter or a number or a letter with an accent or a space
-    const decodedMessage = iconv.decode(slicedBuffer, 'utf16le').replace(/[^a-zA-Z0-9\u00C0-\u00FF ]/g, '');
+    // remove any character that is not a letter or a number or a letter with an accent or space
+
+
+    const decodedMessage = iconv.decode(slicedBuffer, 'utf16').replace(/[^a-zA-Z0-9À-ÿ]/g, '');
+
+
     console.log('Decoded Message:' + decodedMessage);
 
     return decodedMessage;
