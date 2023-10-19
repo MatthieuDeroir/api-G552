0229904c8c90c63735eb0ca2c267c1bc6b360d9b
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-10-19 12:03:04 +0200

    debugging

--- Sockets/Unixsocket.js
+++ Sockets/Unixsocket.js
@@ -212,7 +212,7 @@ module.exports = {
     },
     sendData: function (data) {
         // console.log('UNIX Socket is sending scoring')
-        console.log(data)
+        // console.log(data)
         sharedEmitter.emit('data-received', data);
     },
     sendMedia: function (data) {
