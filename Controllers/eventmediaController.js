b81f37f55554f8172ce5d3edc21d3b7a6407a18d
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-10-19 12:04:53 +0200

    debugging

--- Controllers/eventmediaController.js
+++ Controllers/eventmediaController.js
@@ -16,7 +16,7 @@ class EventMediaController {
       .then((media) => {
         if (media && media.type === "video") {
           // Obtenir la durée de la vidéo en utilisant fluent-ffmpeg
-          ffmpeg.ffprobe('../../Server/Frontend/public/'+media.path, (err, metadata) => {
+          ffmpeg.ffprobe('../../Server/Frontend/build'+media.path, (err, metadata) => {
             if (err) {
               console.log(err);
               res.status(500).json({ message: err });
